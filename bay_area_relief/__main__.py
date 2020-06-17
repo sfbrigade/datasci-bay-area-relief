fcff8bceb89b1162eee41351e29b0584dddddd40
adamos486 <adamos486@gmail.com>
2020-06-17 13:32:07 -0700

    Initial SQLAlchemy table

--- bay_area_relief/__main__.py
+++ bay_area_relief/__main__.py
@@ -1,9 +1,76 @@
+import enum
+
 from flask import Flask
 from bay_area_relief.search import search_bp
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/bar"
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class UniqueCountyEnum(enum.Enum):
+    yes = 'Yes'
+    no = 'No'
+    unknown = 'Unknown'
+
+
+class CountyEnum(enum.Enum):
+    all = 'All'
+    some = 'Some'
+    unknown = 'Unknown'
+
+
+class CategoryEnum(enum.Enum):
+    agriculture = 'Agriculture'
+    all = 'All'
+    unknown = 'Unknown'
+    multiple = 'Multiple'
+    healthcare = 'Healthcare'
+    entertainment = 'Entertainment'
+    hospitality = 'Hospitality'
+    manufacturing = 'Manufacturing'
+    education = 'Education'
+    mass_media = 'Mass Media'
+    software = 'Software'
+    real_estate = 'Real Estate'
+    sport = 'Sport'
+    transport = 'Transport'
+    financial = 'Financial Services'
+
+
+class ReliefModel(db.Model):
+    __tablename__ = 'relief'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    sf_county = db.Column(db.Enum(UniqueCountyEnum))
+    alameda_county = db.Column(db.Enum(UniqueCountyEnum))
+    san_mateo_county = db.Column(db.Enum(UniqueCountyEnum))
+    contra_costa_county = db.Column(db.Enum(UniqueCountyEnum))
+    santa_clara_county = db.Column(db.Enum(UniqueCountyEnum))
+    county = db.Column(db.Enum(CountyEnum))
+    category = db.Column(db.Enum(CategoryEnum))
+
+    def __init__(self, name, sf_county, alameda_county, san_mateo_county, contra_costa_county, santa_clara_county,
+                 county, category):
+        self.name = name
+        self.sf_county = sf_county
+        self.alameda_county = alameda_county
+        self.san_mateo_county = san_mateo_county
+        self.contra_costa_county = contra_costa_county
+        self.santa_clara_county = santa_clara_county
+        self.county = county
+        self.category = category
+
+    def __repr__(self):
+        return "f<Relief {self.name}>"
 
 
 def create_app():
-    app = Flask(__name__)
     app.register_blueprint(search_bp)
     return app
 
